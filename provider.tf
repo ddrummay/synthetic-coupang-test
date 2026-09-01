f34c3c47b0bdf7ef5fd44db1bc69243b54285ba2
ddrummay <48817806+ddrummay@users.noreply.github.com>
2026-09-01 12:08:26 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,15 +9,18 @@ terraform {
 }
 
 variable "new_relic_account_id" {
-  type = string
+  type        = string
+  description = "New Relic Account ID"
 }
 
 variable "new_relic_api_key" {
-  type = string
+  type        = string
+  description = "New Relic User API Key"
+  sensitive   = true
 }
 
 provider "newrelic" {
   account_id = var.new_relic_account_id
   api_key    = var.new_relic_api_key
-  region     = "US" # Change to "EU" if using European New Relic datacenter
+  region     = "US" # Change to "EU" if your account is in the EU region
 }
