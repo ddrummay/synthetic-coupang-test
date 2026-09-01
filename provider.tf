c48a71757d48075840f128edf6f22c6b95bf79d6
ddrummay <48817806+ddrummay@users.noreply.github.com>
2026-09-01 12:06:07 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "~> 3.0"
+    }
+  }
+}
 
+variable "new_relic_account_id" {
+  type = string
+}
+
+variable "new_relic_api_key" {
+  type = string
+}
+
+provider "newrelic" {
+  account_id = var.new_relic_account_id
+  api_key    = var.new_relic_api_key
+  region     = "US" # Change to "EU" if using European New Relic datacenter
+}
